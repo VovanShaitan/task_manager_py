fbfa2cbfe2faedcef086d8c8884a1056eeaa6483
Vladimir-Fedorov <neroazzurri@mail.ru>
2022-11-27 05:13:04 +0400

    Edited task.py

--- main/models/task.py
+++ main/models/task.py
@@ -3,18 +3,6 @@ from django_fsm import FSMField, transition
 from main.models.user import User
 from main.models.tag import Tag
 
-STATES = (
-    "new_task",
-    "in_development",
-    "in_qa",
-    "in_code_review",
-    "ready_for_release",
-    "released",
-    "archived",
-)
-STATES = list(zip(STATES, STATES))
-
-
 class Task(models.Model):
     """Model definition for Task."""
 
@@ -23,11 +11,23 @@ class Task(models.Model):
         BY_MORNING = "by_morning"
         ON_MONDAY = "on_Monday"
 
+    STATES = (
+    "new_task",
+    "in_development",
+    "in_qa",
+    "in_code_review",
+    "ready_for_release",
+    "released",
+    "archived",
+    )
+
+    STATES = list(zip(map(str.upper, STATES), STATES))
+
     title = models.CharField(max_length=255, blank=False)
     description = models.TextField(blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    expired_at = models.DateField(blank=False)
+    expired_at = models.DateField()
     state = FSMField(default=STATES[0], choices=STATES)
     priority = models.CharField(
         max_length=255, default=Priority.IMMEDIATELY, choices=Priority.choices
