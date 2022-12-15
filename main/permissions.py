02e760f3898c8be421a9852de0d82ab202ac37b9
Vladimir-Fedorov <neroazzurri@mail.ru>
2022-12-15 14:59:20 +0400

    Reformatted code by black

--- main/permissions.py
+++ main/permissions.py
@@ -2,11 +2,11 @@ from rest_framework.permissions import BasePermission
 
 
 class OnlyStaffCanDeletedObjectPermission(BasePermission):
-    message = 'Only the administrator can delete.'
+    message = "Only the administrator can delete."
 
     def has_permission(self, request, view):
         return bool(request.user and request.user.is_authenticated)
-    
+
     def has_object_permission(self, request, view, obj):
         if request.method != "DELETE":
             return True
